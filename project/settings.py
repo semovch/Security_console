eed71adf717c3a8641bb029a4e03af79d9e83949
semovch <107846695+semovch@users.noreply.github.com>
2022-12-08 16:29:50 +0600

    Вынес настройки в переменные окружения

--- project/settings.py
+++ project/settings.py
@@ -1,43 +1,46 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv('.env')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': os.getenv('ENGINE'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('SERVER_USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
-INSTALLED_APPS = ['datacenter']
+INSTALLED_APPS = [os.getenv('INSTALLED_APPS')]
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = os.getenv('ROOT_URLCONF')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        'BACKEND': os.getenv('BACKEND'),
+        'DIRS': [os.path.join(BASE_DIR, os.getenv('DIRS'))],
+        'APP_DIRS': os.getenv('APP_DIRS'),
     },
 ]
 
 
-USE_L10N = True
+USE_L10N = os.getenv('USE_L10N')
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = os.getenv('TIME_ZONE')
 
-USE_TZ = True
+USE_TZ = os.getenv('USE_TZ')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = os.getenv('DEFAULT_AUTO_FIELD')
